ba5242fd2f29521e78c2b7b993e5226b5bfc41ba
Nordfin <itdev@nordfincapital.com>
2021-02-25 08:41:36 +0100

    Stopped Invoice Report Added

--- Nordfin/frmReports.aspx.designer.cs
+++ Nordfin/frmReports.aspx.designer.cs
@@ -87,6 +87,15 @@ namespace Nordfin
         protected global::System.Web.UI.WebControls.Button btnContested;
 
         /// <summary>
+        /// btnStopped control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnStopped;
+
+        /// <summary>
         /// txtFromDate control.
         /// </summary>
         /// <remarks>
